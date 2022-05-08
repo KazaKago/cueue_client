ae4f2cecf1ad6d9ac584959a878058b10a6d2d91
Kensuke Tamura <kazakago@gmail.com>
2022-05-08 17:52:18 +0900

    add return

--- lib/data/api/hierarchy/tag/order_tag_api.dart
+++ lib/data/api/hierarchy/tag/order_tag_api.dart
@@ -1,6 +1,7 @@
 import 'package:cueue/data/api/global/requester/dio_creator.dart';
 import 'package:cueue/data/api/global/requester/error_classifier.dart';
 import 'package:cueue/data/api/request/tag/tag_order_request.dart';
+import 'package:cueue/data/api/response/tag/tag_response.dart';
 import 'package:dio/dio.dart';
 
 class OrderTagApi {
@@ -8,10 +9,13 @@ class OrderTagApi {
 
   final DioCreator _dioCreator;
 
-  Future<void> execute(int workspaceId, TagOrderRequest request) async {
+  Future<List<TagResponse>> execute(int workspaceId, TagOrderRequest request) async {
     try {
       final dio = await _dioCreator.create();
-      await dio.patch<void>('/$workspaceId/tags/order', data: request);
+      final response = await dio.patch<List<dynamic>>('/$workspaceId/tags/order', data: request);
+      return response.data!.map<TagResponse>((dynamic json) {
+        return TagResponse.fromJson(json as Map<String, dynamic>);
+      }).toList();
     } on DioError catch (dioError) {
       throw dioError.parseException();
     }
