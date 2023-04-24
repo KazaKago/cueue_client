cc9fa62877bb955cfd70e61746e253f64ac95985
Kensuke Tamura <kazakago@gmail.com>
2023-04-24 21:23:42 +0900

    update dependencies

--- lib/legacy/data/cache/hierarchy/menu/menu_state_manager.dart
+++
@@ -1,5 +0,0 @@
-import 'package:cueue/legacy/data/cache/hierarchy/cache.dart';
-import 'package:cueue/model/menu/menu_id.dart';
-import 'package:store_flowable/store_flowable.dart';
-
-class MenuStateManager extends FlowableDataStateManager<MenuId> implements Cache {}
